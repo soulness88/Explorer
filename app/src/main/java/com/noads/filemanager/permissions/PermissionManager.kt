4c3f5d47eda318ce2b17de607fee9318ef63deb3
soulness88 <juneazpiri@gmail.com>
2026-06-03 12:06:04 +0200

    cambio estructura para actions

--- app/src/main/java/com/noads/filemanager/permissions/PermissionManager.kt
+++ app/src/main/java/com/noads/filemanager/permissions/PermissionManager.kt
@@ -1,26 +1,57 @@
 package com.noads.filemanager.permissions
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Environment
 import android.provider.Settings
+import androidx.core.content.ContextCompat
 
 object PermissionManager {
 
-    fun hasPermission(): Boolean {
+    fun hasPermission(
+        context: Context
+    ): Boolean {
 
-        return Environment.isExternalStorageManager()
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+
+            Environment.isExternalStorageManager()
+
+        } else {
+
+            val readGranted =
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+
+            val writeGranted =
+                ContextCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+
+            readGranted && writeGranted
+        }
     }
 
     fun requestPermission(
         context: Context
     ) {
 
-        val intent =
-            Intent(
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+
+            val intent = Intent(
                 Settings.ACTION_MANAGE_ALL_FILES_ACCESS_PERMISSION
             )
 
-        context.startActivity(intent)
+            intent.addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK
+            )
+
+            context.startActivity(intent)
+        }
     }
 }
